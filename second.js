1c60817bd7a78dc3f649405f0396372491c8553e
Brooke Lewis <belv2c@gmail.com>
2017-07-24 21:40:02 -0500

    sonnet exercises

--- second.js
+++ second.js
@@ -1,1 +1,22 @@
 console.log("I'm in second.js");
+
+var perry = "penguin";
+var nellie = "owl";
+
+console.log("Perry is a " + perry);
+
+//Built in Methods
+// .indexOf()
+// .charAt()
+// .replace()
+
+quote.indexOf("things")); //will give
+
+console.log(quote.charAt(0));
+
+console.log(document.getElemendById("secondQuote"));
+
+
+var element = document.getElementById("secondQuote");
+var jsSString = element.innerHTML;
+console.log(jsString);
